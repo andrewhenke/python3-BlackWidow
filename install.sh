3349f6e8074aa09bed36972ff8b5ec1de6a04d47
root <root@kali>
2020-11-19 09:47:05 -0500

    BlackWidow for Python3

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Install script for blackwidow & injectx fuzzer
+# Crated by @xer0dayz - https://xerosecurity.com
+
+OKBLUE='\033[94m'
+OKRED='\033[91m'
+OKGREEN='\033[92m'
+OKORANGE='\033[93m'
+RESET='\e[0m'
+
+echo -e "$OKRED $RESET"
+echo -e "$OKRED $RESET"
+echo -e "$OKRED                  _.._$RESET"
+echo -e "$OKRED                .'    '.$RESET"
+echo -e "$OKRED               /   __   \ $RESET"
+echo -e "$OKRED            ,  |   ><   |  ,$RESET"
+echo -e "$OKRED           . \  \      /  / .$RESET"
+echo -e "$OKRED            \_'--\`(  )'--'_/$RESET"
+echo -e "$OKRED              .--'/()\'--.$RESET"
+echo -e "$OKRED  @xer0dayz  /  /\` '' \`\  \ $RESET"
+echo -e "$OKRED               |        |$RESET"
+echo -e "$OKRED                \      /$RESET"
+echo -e "$OKRED $RESET"
+echo ""
+echo -e "$OKRED + -- --=[ https://xerosecurity.com $RESET"
+echo -e "$OKRED + -- --=[ blackwidow by @xer0dayz $RESET"
+echo ""
+
+echo -e "$OKBLUE[*]$RESET Installing BlackWidow... $RESET"
+apt update
+apt install -y python3 python3-requests python3-pip python3-lxml python3-requests openssl ca-certificates python3-dev wget git
+cp -f $PWD/blackwidow /usr/bin/blackwidow
+cp -f $PWD/injectx.py /usr/bin/injectx.py
+cp -f $PWD/blackwidow.desktop /usr/share/applications/
+
+echo -e "$OKBLUE[*]$RESET Done! $RESET"
+echo -e "$OKRED[>]$RESET To run, type 'blackwidow'! $RESET"
